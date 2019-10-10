d245b233989a49f08c094dae0cb66b06b2b301c3
Justin Winter <justinlevi@gmail.com>
2019-10-10 08:27:48 -0400

    yarn upgrade - addressing security issue noted in github

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHello_1 = __importDefault(require("./sayHello"));
-const result = sayHello_1.default({ name: "y'all" });
-console.log(result);
+const cheerio = __importStar(require("cheerio"));
+const axios_1 = __importDefault(require("axios"));
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const { data: html } = yield axios_1.default.get('https://ny.gov');
+    const $ = cheerio.load(html);
+    const hrefs = [];
+    $('a').each((index, { attribs: { href } }) => {
+        if (!href) {
+            return;
+        }
+        if (href && !href.startsWith('#') && !href.startsWith('http')) {
+            hrefs.push(href);
+        }
+    });
+    console.log(hrefs);
+});
+main();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
-import sayHello from "./sayHello";
+import * as cheerio from 'cheerio'
+import axios from 'axios'
 
-const result = sayHello({ name: "y'all" });
-console.log(result);
+const main = async () => {
+  const { data: html } = await axios.get('https://ny.gov')
+  const $ = cheerio.load(html)
+  const hrefs: String[] = []
+  $('a').each((index, { attribs: { href } }) => {
+    if (!href) {
+      return
+    }
+    if (href && !href.startsWith('#') && !href.startsWith('http')) {
+      hrefs.push(href)
+    }
+  })
+  console.log(hrefs)
+}
 
+main()
